0c266fa296ada6e04d02acf6cad4f459b311093e
Aneta <sopaaneta@gmail.com>
2023-05-21 22:43:10 +0200

    Style subtotal and total number of prices

--- app/card/page.js
+++ app/card/page.js
@@ -16,51 +16,69 @@ export const dynamic = 'force-dynamic';
 
 export default function CardPage() {
   return (
-    <main className={styles.container}>
-      <h1>Products Card</h1>
-      <div className={styles.containerCardProducts}>
-        <div className={styles.productsCardsContainer}>
-          {products.map((product) => {
-            return (
-              <div
-                key={`product-div-${product.id}`}
-                className={styles.productCard}
-              >
-                <Image
-                  className={styles.img}
-                  src={`/images/${product.name}.png`}
-                  width={150}
-                  height={100}
-                />
-                <div className={styles.productsTexts}>
-                  <Link
-                    className={styles.productName}
-                    href={`/products/${product.id}`}
-                    style={{
-                      textDecoration: 'none',
-                    }}
+    <main>
+      <div className={styles.container}>
+        <h1>Products Card</h1>
+        <div className={styles.containerCardProducts}>
+          <div className={styles.productsCardsContainer}>
+            {products
+              .map((product) => {
+                return (
+                  <div
+                    key={`product-div-${product.id}`}
+                    className={styles.productCard}
                   >
-                    {product.name}
-                  </Link>
-                  <p className={styles.productType}>{product.type}</p>
-                  <p className={styles.productPrice}>
-                    {product.price} <span>Euro</span>{' '}
-                  </p>
-                  <QuantityCounter />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.imageContainer}>
-          <Image
-            data-test-id="product-image"
-            className={styles.img}
-            alt="productName"
-            width={250}
-            height={250}
-            src={image}
-          />
+                    <Image
+                      className={styles.img}
+                      src={`/images/${product.name}.png`}
+                      width={150}
+                      height={100}
+                    />
+                    <div>
+                      <Link
+                        className={styles.productName}
+                        href={`/products/${product.id}`}
+                        style={{
+                          textDecoration: 'none',
+                        }}
+                      >
+                        {product.name}
+                      </Link>
+                      <p className={styles.productType}>{product.type}</p>
+                      <p className={styles.productPrice}>
+                        {product.price} <span>Euro</span>{' '}
+                      </p>
+                      <QuantityCounter />
+                    </div>
+                  </div>
+                );
+              })
+              .splice(0, 3)}
+          </div>
+          <div className={styles.sumContainer}>
+            <h2>Summary of the amount</h2>
+            <div className={styles.subtotal}>
+              <h5>Subtotal</h5>
+              <p>18 Euro</p>
+            </div>
+            <div className={styles.total}>
+              <h3>Total</h3>
+              <p>
+                <span>18</span> Euro
+              </p>
+            </div>
+            <Link
+              data-test-id="product-add-to-cart"
+              className={styles.button}
+              href="/checkout"
+              style={{
+                color: 'black',
+                textDecoration: 'none',
+              }}
+            >
+              Checkout
+            </Link>
+          </div>
         </div>
       </div>
     </main>
--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
-import { getProductById } from '../../../database/products';
+import { getProductById, products } from '../../../database/products';
 import styles from './page.module.scss';
 import QuantityCounter from './QuantityCounter';
 
@@ -74,6 +74,57 @@ export default function ProductPage(props: Props) {
             src={`/images/${singleProduct.name}.png`}
           />
         </div>
+      </div>
+
+      <div className={styles.productsCardsContainer}>
+        {products.map((product) => {
+          return (
+            <div
+              key={`product-div-${product.id}`}
+              className={styles.productCard}
+            >
+              {' '}
+              {/* <Image
+                src={`/images/${product.name}.png`}
+                width={250}
+                height={200}
+              /> */}
+              <div className={styles.container}>
+                <Image
+                  src={`/images/${product.name}.png`}
+                  width={250}
+                  height={200}
+                />
+                <div className={styles.overlay}></div>
+                <div className={styles.button}>
+                  <Link
+                    className={styles.a}
+                    href={`/products/${product.id}`}
+                    style={{
+                      color: 'black',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    OPEN
+                  </Link>
+                </div>
+              </div>
+              <Link
+                className={styles.productName}
+                href={`/products/${product.id}`}
+                style={{
+                  textDecoration: 'none',
+                }}
+              >
+                {product.name}
+              </Link>
+              <p className={styles.productType}>{product.type}</p>
+              <p className={styles.productPrice}>
+                {product.price} <span>Euro</span>{' '}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
