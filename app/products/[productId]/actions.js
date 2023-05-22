011720d2f04c8c6029cc548242684e21b490600c
Aneta <sopaaneta@gmail.com>
2023-05-22 13:23:01 +0200

    Fix the error about extencions

--- app/products/[productId]/actions.js
+++ app/products/[productId]/actions.js
@@ -1,27 +1,28 @@
-// import { cookies } from 'next/headers';
-// import { getCookie } from '../../../util/cookies';
-// import { parseJson } from '../../../util/json';
-// import useServer from './useServer';
+'use server';
 
-// export async function createOrUpdateQuantity(productId, quantity) {
-//   const productQuantityCookie = getCookie('productQuantity');
-//   const productQuantities = !productQuantityCookie
-//     ? []
-//     : parseJson(productQuantityCookie);
+import { cookies } from 'next/headers';
+import { getCookie } from '../../../util/cookies';
+import { parseJson } from '../../../util/json';
 
-//   const quantityToUpdate = productQuantities.find((quantityNumber) => {
-//     return quantityNumber.id === productId;
-//   });
+export async function createOrUpdateQuantity({ productId, quantity }) {
+  const productQuantityCookie = getCookie('productQuantity');
+  const productQuantities = !productQuantityCookie
+    ? []
+    : parseJson(productQuantityCookie);
 
-//   if (quantityToUpdate) {
-//     quantityToUpdate.quantity =
-//       Number(quantityToUpdate.quantity) + Number(quantity);
-//   } else {
-//     productQuantities.push({
-//       id: productId,
-//       quantity,
-//     });
-//   }
+  const quantityToUpdate = productQuantities.find((quantityNumber) => {
+    return quantityNumber.id === productId;
+  });
 
-//   await cookies().set('productQuantity', JSON.stringify(productQuantities));
-// }
+  if (quantityToUpdate) {
+    quantityToUpdate.quantity =
+      Number(quantityToUpdate.quantity) + Number(quantity);
+  } else {
+    productQuantities.push({
+      id: productId,
+      quantity,
+    });
+  }
+
+  await cookies().set('productQuantity', JSON.stringify(productQuantities));
+}
