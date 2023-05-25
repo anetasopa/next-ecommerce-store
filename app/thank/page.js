d980d014e1c9b5fc5977df1b9c1bd5e0e7f794ba
Aneta <sopaaneta@gmail.com>
2023-05-25 15:04:21 +0200

    Add thank page

--- app/thank/page.js
+++ app/thank/page.js
@@ -1,3 +1,5 @@
+import styles from './page.module.scss';
+
 export const metadata = {
   title: 'Thank You | Coffez',
   description: 'Thank you for the order',
@@ -5,8 +7,20 @@ export const metadata = {
 
 export default function ThankPage() {
   return (
-    <main>
-      <h1>Thank you for your order</h1>
+    <main className={styles.container}>
+      <div className={styles.containerText}>
+        <h1>
+          <span>Thank</span> you for your order.
+        </h1>
+        <p>
+          We will begin processing your order immediately.
+          <br />
+          <br />
+          If you have any questions or need assistance, please don't hesitate to
+          reach out to us at the provided contact number:
+        </p>
+        <p className={styles.number}>+(43)-655-769-8876</p>
+      </div>
     </main>
   );
 }
