c637daa3330a52a0eae10e9d80d08def74093482
Aneta <sopaaneta@gmail.com>
2023-06-01 21:41:21 +0200

    Add unit test for the card sum

--- util/__test__/totalSum.test.ts
+++ util/__test__/totalSum.test.ts
@@ -1,11 +1,18 @@
 import { expect, test } from '@jest/globals';
 import { sum } from '../../app/card/Sum';
 
-test('Testing total sum', () => {
-  const subTotal = 10;
-  const newValue = 5;
+test('Testing sum function', () => {
+  expect(sum(5, 3)).toBe(8);
+  expect(sum(0, 7)).toBe(7);
+  expect(sum(10, -5)).toBe(5);
+  expect(sum(0, 0)).toBe(0);
+});
 
-  const result = sum(subTotal, newValue);
-
-  expect(result).toBe(15);
+test('Throws an error', () => {
+  // @ts-expect-error testing incorrect arguments
+  expect(() => sum(1, '1')).toThrow('Pass only numbers!');
+  // @ts-expect-error testing incorrect arguments
+  expect(() => sum('asd', '1')).toThrow('Pass only numbers!');
+  // @ts-expect-error testing incorrect arguments
+  expect(() => sum(1, false)).toThrow('Pass only numbers!');
 });
