960bfab5dedd424265b7d62551afdfb6d9c1a654
Aneta <sopaaneta@gmail.com>
2023-06-06 07:32:08 +0200

    Fix the errors from preflight

--- app/checkout/Summary.js
+++ app/checkout/Summary.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import SelectProducts from './SelectProducts';
 import styles from './Summary.module.scss';
 
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { getProducts } from '../../database/products';
 import Filter from './Filter';
 import styles from './page.module.scss';
