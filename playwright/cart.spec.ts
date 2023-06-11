99ac12d7c7837fe3ca8efd1f743cc85d62547ed2
Aneta <sopaaneta@gmail.com>
2023-06-11 22:44:35 +0200

    Add hard timeout to wait until product is added to cart

--- playwright/cart.spec.ts
+++ playwright/cart.spec.ts
@@ -2,19 +2,15 @@ import { expect, test } from '@playwright/test';
 
 test('cart test', async ({ page }) => {
   test.setTimeout(20000);
+
   await page.goto('http://localhost:3000/products/13');
 
-  await expect(page.getByTestId('product-add-to-cart')).toBeVisible();
-
   await page.getByTestId('product-add-to-cart').click();
-
-  await expect(page.getByTestId('cart-count')).toContainText('1');
-
-  await page.getByTestId('product-add-to-cart').click();
+  await page.waitForTimeout(3000);
 
   await page.goto('http://localhost:3000/card');
 
-  await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible();
+  await expect(await page.getByTestId('cart-count')).toContainText('1');
   await page.getByRole('button', { name: 'Remove' }).click();
   await expect(page.getByTestId('cart-count')).toContainText('0');
 });
