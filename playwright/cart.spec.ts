ce6b9de70f05de3c8b58d07616cf0a76dbe52d34
Aneta <sopaaneta@gmail.com>
2023-06-12 00:33:23 +0200

    Tests

--- playwright/cart.spec.ts
+++ playwright/cart.spec.ts
@@ -1,20 +1,29 @@
 import { expect, test } from '@playwright/test';
 
 test('cart test', async ({ page }) => {
-  await page.goto('http://localhost:3000/products/13');
-  // await expect(page).toHaveURL('http://localhost:3000/products/13');
-
-  await expect(page.getByTestId('product-add-to-cart')).toBeVisible();
+  await page.goto('http://localhost:3000/products/1');
 
   await page.getByTestId('product-add-to-cart').click();
+  await expect(page.getByTestId('cart-count').getByText('1')).toBeVisible();
 
-  await expect(page.getByTestId('cart-count')).toContainText('1');
-
+  await page.getByTestId('product-quantity').click();
+  await page.getByTestId('product-quantity').fill('3');
   await page.getByTestId('product-add-to-cart').click();
+  await expect(page.getByTestId('cart-count').getByText('4')).toBeVisible();
 
   await page.goto('http://localhost:3000/cart');
 
-  await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible();
-  await page.getByRole('button', { name: 'Remove' }).click();
-  await expect(page.getByTestId('cart-count')).toContainText('0');
+  // Expect 1 cart items
+  await expect(
+    page
+      .locator('[data-test-id^="cart-product-"]')
+      .filter({
+        has: page.locator('[data-test-id^="cart-product-quantity-"]'),
+      })
+      .filter({
+        has: page.locator('[data-test-id^="cart-product-remove-"]'),
+      }),
+  ).toHaveCount(1);
+
+  await expect(page.getByTestId('cart-count')).toContainText('4');
 });
